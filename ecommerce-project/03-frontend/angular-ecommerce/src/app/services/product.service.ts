7bbe9c5c6c8774cec99f1185c57eeb36cd38f005
omar <omar1234u@gmail.com>
2024-01-01 17:05:15 +0200

    Angular-Spring-boot-ecommerce

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  private categoryUrl = 'http://localhost:8080/api/product-category';
+
+  constructor(private httpClient : HttpClient) { }
+
+  // returns an observable , Map the JSON data from Spring Data REST to Product array
+  getProductList(theCategoryId:number): Observable<Product[]>{
+
+      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+      return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+        map(response => response._embedded.products)
+      );
+  }
+
+  getProductCategories() :Observable<ProductCategory[]> {
+
+      return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+        map(response => response._embedded.productCategory)
+      ); 
+
+  }
+
+}
+
+ // unwraps the json from spring data rest _embedded entry
+ interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  } 
+}
+
+ interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
+}
+
+
